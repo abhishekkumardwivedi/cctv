f90e06cb39cc2230c8f5c32ba843df198f96193f
Abhishek Dwivedi <abhishekkumardwivedi@gmail.com>
2017-10-18 22:07:53 +0530

    tensorflow for things reference

---
+++ references/tf/sample-tensorflow-imageclassifier/app/src/main/java/com/example/androidthings/imageclassifier/ImageClassifierActivity.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2017 The Android Things Samples Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androidthings.imageclassifier;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.androidthings.imageclassifier.classifier.Classifier;
+import com.example.androidthings.imageclassifier.classifier.TensorFlowImageClassifier;
+import com.google.android.things.contrib.driver.button.ButtonInputDriver;
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ImageClassifierActivity extends Activity implements ImageReader.OnImageAvailableListener {
+    private static final String TAG = "ImageClassifierActivity";
+
+    private ImagePreprocessor mImagePreprocessor;
+    private CameraHandler mCameraHandler;
+    private TensorFlowImageClassifier mTensorFlowClassifier;
+
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
+
+    private ImageView mImage;
+    private TextureView mTextureView;
+    private TextView[] mResultViews;
+
+    private AtomicBoolean mReady = new AtomicBoolean(false);
+    private ButtonInputDriver mButtonDriver;
+    private Gpio mReadyLED;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_camera);
+        mImage = (ImageView) findViewById(R.id.imageView);
+        mTextureView = (TextureView)findViewById(R.id.texture);
+        mResultViews = new TextView[3];
+        mResultViews[0] = (TextView) findViewById(R.id.result1);
+        mResultViews[1] = (TextView) findViewById(R.id.result2);
+        mResultViews[2] = (TextView) findViewById(R.id.result3);
+
+        init();
+    }
+
+    private void init() {
+        initPIO();
+
+        mBackgroundThread = new HandlerThread("BackgroundThread");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+        mBackgroundHandler.post(mInitializeOnBackground);
+    }
+
+    private void initPIO() {
+        PeripheralManagerService pioService = new PeripheralManagerService();
+        try {
+            mReadyLED = pioService.openGpio(BoardDefaults.getGPIOForLED());
+            mReadyLED.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            mButtonDriver = null;
+            Log.w(TAG, "Could not open GPIO pins", e);
+        }
+    }
+
+    private Runnable mInitializeOnBackground = new Runnable() {
+        @Override
+        public void run() {
+            mImagePreprocessor = new ImagePreprocessor();
+            mCameraHandler = CameraHandler.getInstance();
+            mCameraHandler.initializeCamera(
+                    ImageClassifierActivity.this, mBackgroundHandler,
+                    ImageClassifierActivity.this,
+                    mTextureView);
+
+            mTensorFlowClassifier = new TensorFlowImageClassifier(ImageClassifierActivity.this);
+
+            setReady(true);
+        }
+    };
+
+    private void setReady(boolean ready) {
+        mReady.set(ready);
+        if (mReadyLED != null) {
+            try {
+                mReadyLED.setValue(ready);
+            } catch (IOException e) {
+                Log.w(TAG, "Could not set LED", e);
+            }
+        }
+    }
+
+    @Override
+    public void onImageAvailable(ImageReader reader) {
+        final Bitmap bitmap;
+        try (Image image = reader.acquireNextImage()) {
+            bitmap = mImagePreprocessor.preprocessImage(image);
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mImage.setImageBitmap(bitmap);
+            }
+        });
+
+        final List<Classifier.Recognition> results = mTensorFlowClassifier.doRecognize(bitmap);
+
+        Log.d(TAG, "Got the following results from Tensorflow: " + results);
+
+            setReady(true);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < mResultViews.length; i++) {
+                    if (results.size() > i) {
+                        Classifier.Recognition r = results.get(i);
+                        mResultViews[i].setText(r.getTitle() + " : " + r.getConfidence().toString());
+                    } else {
+                        mResultViews[i].setText(null);
+                    }
+                }
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            if (mBackgroundThread != null) mBackgroundThread.quit();
+        } catch (Throwable t) {
+            // close quietly
+        }
+        mBackgroundThread = null;
+        mBackgroundHandler = null;
+
+        try {
+            if (mCameraHandler != null) mCameraHandler.shutDown();
+        } catch (Throwable t) {
+            // close quietly
+        }
+        try {
+            if (mTensorFlowClassifier != null) mTensorFlowClassifier.destroyClassifier();
+        } catch (Throwable t) {
+            // close quietly
+        }
+        try {
+            if (mButtonDriver != null) mButtonDriver.close();
+        } catch (Throwable t) {
+            // close quietly
+        }
+    }
+
+}
